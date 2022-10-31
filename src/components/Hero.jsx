8c0ec763adba6a4b282d6bf252a5cb6d959d3174
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-10-31 00:09:17 -0300

    primera vista casi terminada

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { fondoHome } from "../assets";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 const Hero = () => {
   return (
     <>
-{/*     <div className="absolute inset-0 w-full bg-slate-600 bg-opacity-75"></div>
- */}    <div
-      className="flex flex-col w-full bg-green-500 overflow-hidden relative h-[800px]"
-      >
-        <img src={fondoHome} alt="" className="absolute inset-0 w-full h-full object-cover" />
+      <div className="flex flex-col w-full bg-black overflow-hidden relative h-[600px] md:h-[800px]">
+        <img
+          src={fondoHome}
+          alt=""
+          className="absolute inset-0 w-full h-full object-cover"
+        />
         <div className="absolute inset-0 bg-gray-900 bg-opacity-40"></div>
         <div className="flex h-full justify-center relative flex-col items-center">
-        <h1 className="font-poppins text-3xl text-white tracking-wider">CNEL. DORREGO</h1>
-        <h4 className="font-poppins text-xl text-white tracking-wider text-center">EXPLORADORES ARGENTINOS DE DON BOSCO</h4>
+          <h1 className="font-poppins text-3xl md:text-8xl  text-white tracking-wider text-center">
+            CNEL. DORREGO
+          </h1>
+          <h4 className="font-poppins text-xl md:text-4xl text-white tracking-wider text-center">
+            EXPLORADORES ARGENTINOS DE DON BOSCO
+          </h4>
+          <button className="font-poppins bg-sky-900 absolute  md:right-10 bottom-10 w-60 md:w-96 h-20 font-bold text-2xl md:text-4xl text-white tracking-wide">
+            Contactanos <FontAwesomeIcon icon={faEnvelope} />
+          </button>
         </div>
-    </div>
+      </div>
     </>
   );
 };
